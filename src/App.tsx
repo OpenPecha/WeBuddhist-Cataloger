cd797d74dfff7310190b60a9e0448d0f2c5abbcb
TenzDelek <tibetdelek@gmail.com>
2025-12-22 12:13:58 +0530

    move to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./routes/Login/Login.tsx";
+import Text from "./routes/Text/Text.tsx";
 
 function App() {
   return (
     <div className="flex w-full">
       <div className="flex-1 overflow-auto">
-        <Outlet />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Text />} />
+        </Routes>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,40 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Text from "./routes/Text/Text.tsx";
-import Login from "./routes/Login/Login.tsx";
 import { Toaster } from "./components/ui/atoms/sonner.tsx";
 import { Auth0ProviderWithNavigate } from "./providers/Auth0ProviderWithNavigate.tsx";
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/",
-        element: (
-          <Text />
-        ),
-      }
-    ],
-  },
-]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Auth0ProviderWithNavigate>
-        <RouterProvider router={router} />
-        <Toaster />
-      </Auth0ProviderWithNavigate>
+      <BrowserRouter>
+        <Auth0ProviderWithNavigate>
+          <App />
+          <Toaster />
+        </Auth0ProviderWithNavigate>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 );
