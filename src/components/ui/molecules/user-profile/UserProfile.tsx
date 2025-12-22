0cb2f649663dc3364658bdedb4659256d5a5cf7a
TenzDelek <tibetdelek@gmail.com>
2025-12-22 12:58:09 +0530

    logout and userprofile

--- src/components/ui/molecules/user-profile/UserProfile.tsx
+++ src/components/ui/molecules/user-profile/UserProfile.tsx
@@ -1,17 +1,60 @@
-import { UserIcon } from "lucide-react"
+import { useAuth0 } from "@auth0/auth0-react"
+import { LogOut } from "lucide-react"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/atoms/dropdown-menu"
+
 const UserProfile = () => {
+    const { user, logout } = useAuth0()
+
     return (
-        <div className=" space-x-4 flex justify-between items-center w-full">
-            <div className="flex items-center md:space-x-2">
-                <div className=" bg-white border border-sidebar-border p-2 rounded-full">
-                    <UserIcon className="w-4 h-4" />
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <div className="flex justify-between items-center w-full cursor-pointer ">
+                    <div className="flex items-center md:space-x-2">
+                        <div className="bg-white border border-edge rounded-full">
+                            <img
+                                src={user?.picture}
+                                alt="user-profile"
+                                className="w-10 h-10 rounded-full"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div className="hidden md:flex text-sm flex-col">
-                    <span >Tenzin Delek</span>
-                    <span className="text-xs text-text-custom">tenzin@gmail.com</span>
-                </div>
-            </div>
-        </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-56 rounded-none">
+                <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                        <p className="text-sm font-medium leading-none">{user?.name}</p>
+                        <p className="text-xs leading-none text-muted-foreground">
+                            {user?.email}
+                        </p>
+                    </div>
+                </DropdownMenuLabel>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem
+                    className="cursor-pointer rounded-none"
+                    onClick={() =>
+                        logout({
+                            logoutParams: {
+                                returnTo: window.location.origin + "/login",
+                            },
+                        })
+                    }
+                >
+                    <LogOut className="mr-2 h-4 w-4" />
+                    Logout
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
 
