c451419593ec3badc18e64145059784c5346aa21
TenzDelek <tibetdelek@gmail.com>
2025-12-22 12:19:24 +0530

    call

--- src/config/axios-config.ts
+++ src/config/axios-config.ts
@@ -3,7 +3,9 @@ import {
   ACCESS_TOKEN,
 } from "@/lib/constants";
 
-const axiosInstance = axios.create({});
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
+  });
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = sessionStorage.getItem(ACCESS_TOKEN);
