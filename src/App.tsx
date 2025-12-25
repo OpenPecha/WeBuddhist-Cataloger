194b35e9fdec6a9ff78446257ecb59ae67da55de
TenzDelek <tibetdelek@gmail.com>
2025-12-25 16:12:32 +0530

    Implement lazy loading for Text and Instance components with a fallback loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./routes/Login/Login.tsx";
-import Text from "./routes/Text/Text.tsx";
 import { AuthenticationGuard } from "./providers/AuthGuard.tsx";
-import Instance from "./routes/Instance/Instance.tsx";
+import { lazy, Suspense } from "react";
+const Text = lazy(() => import("./routes/Text/Text.tsx"));
+const Instance = lazy(() => import("./routes/Instance/Instance.tsx"));
 
 function App() {
   return (
     <div className="flex w-full">
       <div className="flex-1 overflow-auto">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<AuthenticationGuard component={Text} />} />
-          <Route
-            path="/instance/:id"
-            element={<AuthenticationGuard component={Instance} />}
-          />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="flex flex-col items-center justify-center h-screen">
+              Loading...
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={<AuthenticationGuard component={Text} />}
+            />
+            <Route
+              path="/instance/:id"
+              element={<AuthenticationGuard component={Instance} />}
+            />
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
