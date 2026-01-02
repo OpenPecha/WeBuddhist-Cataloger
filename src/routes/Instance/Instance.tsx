ec4a42258651b43c34dc546efff0ba5f316150ae
TenzDelek <tibetdelek@gmail.com>
2026-01-02 12:18:40 +0530

    Add text synchronization feature with loading state and improved UI elements

--- src/routes/Instance/Instance.tsx
+++ src/routes/Instance/Instance.tsx
@@ -4,7 +4,7 @@ import { TextDetailDashboard } from "@/components/ui/molecules/dashboard/textdet
 import { Separator } from "@/components/ui/atoms/Seperator";
 import { Button } from "@/components/ui/atoms/button";
 import axiosInstance from "@/config/axios-config";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getReadableAxiosError } from "@/lib/error";
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
@@ -16,7 +16,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/atoms/select";
-import { MoveRightIcon, RotateCcw } from "lucide-react";
+import { Loader2Icon, MoveRightIcon, RotateCcw } from "lucide-react";
 import { Skeleton } from "@/components/ui/atoms/skeleton";
 export const FetchTextDetailInfo = async (textId: string) => {
   const { data } = await axiosInstance.get(`/api/v1/cataloger/texts/${textId}`);
@@ -42,6 +42,25 @@ const Instance = () => {
     refetchOnWindowFocus: false,
   });
 
+  const { mutate: synxText, isPending } = useMutation({
+    mutationFn: () => {
+      return axiosInstance.post(`/api/v1/upload-text/texts/${id}`, {
+        textId: id,
+        source,
+        destination,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Text will be uploaded to WeBuddhist within 24 hours");
+    },
+    onError: (error) => {
+      const info = getReadableAxiosError(error);
+      toast.error(info.title, {
+        description: info.detail,
+        duration: 5000,
+      });
+    },
+  });
   const breadcrumbItems = [
     { label: "Home", path: "/" },
     { label: "Dashboard", path: "/" },
@@ -67,15 +86,16 @@ const Instance = () => {
   return (
     <MainLayout breadcrumbItems={breadcrumbItems}>
       <div className="flex flex-col w-full h-full border-t border-edge">
-        <div className="flex flex-col items-start gap-2 p-4">
+        <div className="flex flex-col items-start gap-4 p-4">
           <span className="text-2xl font-monlam">
             {textdata?.title.en || textdata?.title.bo || (
               <Skeleton className=" w-md h-8" />
             )}{" "}
           </span>
           <div className="flex items-center gap-2">
+            <p className="text-sm text-muted-foreground">Pecha Server:</p>
             <Select value={source} onValueChange={setSource}>
-              <SelectTrigger className="w-[150px]">
+              <SelectTrigger className="w-[200px]">
                 <SelectValue placeholder="Source" />
               </SelectTrigger>
               <SelectContent>
@@ -87,8 +107,9 @@ const Instance = () => {
               </SelectContent>
             </Select>
             <MoveRightIcon className="w-4 h-4 text-gray-400" />
+            <p className="text-sm text-muted-foreground">WeBuddhist:</p>
             <Select value={destination} onValueChange={setDestination}>
-              <SelectTrigger className="w-[150px]">
+              <SelectTrigger className="w-[200px]">
                 <SelectValue placeholder="Destination" />
               </SelectTrigger>
               <SelectContent>
@@ -102,14 +123,15 @@ const Instance = () => {
             <Button
               variant="outline"
               className="cursor-pointer"
-              disabled={!source || !destination || textdata?.status || error}
+              onClick={() => synxText()}
+              disabled={!source || !destination || textdata?.status || error || isPending}
             >
-              <RotateCcw className="w-4 h-4" />
-              Bulk Sync
+              {isPending ? <Loader2Icon className="w-4 h-4 animate-spin" /> : <RotateCcw className="w-4 h-4" />}
+              Sync
             </Button>
           </div>
         </div>
-        <div className="text-xl p-4">Alignment</div>
+        <div className="text-xl p-4">Alignments</div>
         <div className="flex">
           <div className="w-4 h-full">
             <Separator />
